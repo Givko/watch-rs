7d73ef4c50d9c540669cb73cbff25f207e43bc7e
Givko <milevjivko@gmail.com>
2025-07-29 23:55:08 +0300

    Add clap and most simple command execution funcitonality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+use std::process::Command;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Command to execute
+    #[arg(short, long)]
+    command: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let Ok(output) = Command::new(cli.command).status() else {
+        println!("Error");
+        return;
+    };
 }
